09ee8c0553b82cdb879420a8c2911a99276e9641
Tommaso Polvere <tommasopolvere@gmail.com>
2022-11-09 18:17:51 +0100

    Include CPP files which extends .H files to project.DEV

--- Libro.h
+++ Libro.h
@@ -10,53 +10,12 @@ class Libro: public Prodotto{
 		string isbn;
 	public:
 	
-		Libro(){
-			
-			cout<<"Libro Creato"<<endl;
-		}
-		Libro(string titolo,string autore,float prezzo,int quantita,string isbn):Prodotto(titolo,autore,prezzo,quantita){
-			
-			this->isbn=isbn;
-			
-			
-		}	
+		Libro();
+		Libro(string ,string ,float ,int ,string );
+	
 		
-		void setTitolo(string n_titolo){
-			
-			titolo=n_titolo;
-			
-		}
-		
-		void setAutore(string n_autore){
-			
-			autore=n_autore;
-			
-		}
-		
-		void setPrezzo(float n_prezzo){
-			
-			prezzo=n_prezzo;
-		}
-		
-		void setQuantita(int n_quantita){
-			
-			quantita=n_quantita;
-		}
-		
-		void setISBN(string n_isbn){
-			
-			isbn=n_isbn;
-		}
-		
-		string getISBN(){
-			
-			return isbn;
-		}
-		
-		void stampa(){
-			
-			Prodotto::stampa();
-			cout<<"ISBN: "<<isbn<<endl;
-		}
+		void setISBN(string n_isbn);
+		string getISBN();
+		void stampa();
 	
 };
