4d12553e00e8df4588aa492378f38e53ff6a0a63
Tommaso Polvere <tommasopolvere@gmail.com>
2022-11-09 16:14:48 +0100

    Final Main

--- main.cpp
+++ main.cpp
@@ -4,27 +4,59 @@
  *Author:Dontu Victor Darius-Polvere Tommaso
  */ 
 
+#include<cstdlib>
 
 #ifndef ARCHIVIO_H
 #define ARCHIVIO_H
 #include "Archivio.h"
 #endif
 
+#ifndef LIBRO_H
+#define LIBRO_H
+#include "Libro.h"
+#endif
+
+#ifndef EBOOK_H
+#define EBOOK_H
+#include "Ebook.h"
+#endif
+
+#ifndef CD_H
+#define CD_H
+#include "Cd.h"
+#endif
+
+#ifndef VINILE_H
+#define VINILE_H
+#include "Vinile.h"
+#endif
+
+#ifndef CANZONE_H
+#define CANZONE_H
+#include"Canzone.h"
+#endif
+
+bool scelta;
+string titolo,autore,t,isbn;
+float prezzo;
+int quantita,scl,s,cond,condizione;
+double dimensioni,durata;
+
+Prodotto res;
+
+Libro libro(titolo,autore,prezzo,quantita,isbn);
+Vinile vinile(titolo,autore,prezzo,quantita,dimensioni);
+CdAudio cd(titolo,autore,prezzo,quantita);
+Ebook ebook(titolo,autore,prezzo,quantita,isbn);
+
+Canzone canzone(durata,titolo,autore);
+
+
+Archivio archivio;
+Prodotto prodotto (titolo,autore,prezzo,quantita);
 
 
 int main(){
-
-	bool scelta;
-	
-	string titolo,autore;
-	float prezzo;
-	int quantita,scelta;
-
-	Prodotto res;
-
-
-	Archivio archivio;
-	Prodotto prodotto (titolo,autore,prezzo,quantita);
 
 
 	
@@ -35,16 +67,16 @@ int main(){
 			
 			cout<<"----LIBRERIA AVANZATA----"<<endl;
 			cout<<"1 Aggiungi Prodotto      "<<endl;
-			cout<<"2 Stampa Prodotti        "<<endl;
+			cout<<"2 Stampa x categoria     "<<endl;
 			cout<<"3 Ricerca Prodotti       "<<endl;
 			cout<<"4 Rimuovi Prodotto       "<<endl;
 			cout<<"0 Uscire ----->          "<<endl;
 			cout<<"-------------------------"<<endl;
 			cout<<endl;
 			cout<<"Inserire la scelta 0|4   "<<endl;
-			cin>>scelta;
+			cin>>scl;
 			
-			switch(scelta){
+			switch(scl){
 				
 				case 0:
 					
@@ -53,29 +85,143 @@ int main(){
 				
 				case 1:
 				
-					cout<<"Inserire il titolo"<<endl;
-					cin>>titolo;
-					prodotto.setTitolo(titolo);
-					cout<<endl;
-					cout<<"Inserire l'autore'"<<endl;
-					cin>>autore;
-					prodotto.setAutore(autore);
-					cout<<endl;
-					cout<<"Inserire il prezzo"<<endl;
-					cin>>prezzo;
-					prodotto.setPrezzo(prezzo);
-					cout<<endl;
+					cout<<"Quale tipo di prodotto vuoi inserire? 1(libro) 2(Ebook) 3(Cd) 4(Vinile)"<<endl;
+					cin>>cond;
 					
-					prodotto.setQuantita(1);
-					archivio.aggiungiProdotto(prodotto);
+					if(cond==1){
+						
+						
+						cout<<"Inserire il titolo"<<endl;
+						cin>>titolo;
+						libro.setTitolo(titolo);
+						cout<<endl;
+						cout<<"Inserire l'autore'"<<endl;
+						cin>>autore;
+						libro.setAutore(autore);
+						cout<<endl;
+						cout<<"Inserire il prezzo"<<endl;
+						cin>>prezzo;
+						libro.setPrezzo(prezzo);
+						cout<<endl;
+						cout<<"Inserire ISBN "<<endl;
+						cin>>isbn;
+						libro.setISBN(isbn);
+						cout<<endl;
+						
+						libro.setQuantita(1);
+						archivio.addLibro(libro);
+						archivio.addProdotto(libro);
+						
+						
+					}
 					
+					if(cond==2){
+						
+						cout<<"Inserire il titolo"<<endl;
+						cin>>titolo;
+						ebook.setTitolo(titolo);
+						cout<<endl;
+						cout<<"Inserire l'autore'"<<endl;
+						cin>>autore;
+						ebook.setAutore(autore);
+						cout<<endl;
+						cout<<"Inserire il prezzo"<<endl;
+						cin>>prezzo;
+						ebook.setPrezzo(prezzo);
+						cout<<endl;
+						cout<<"Inserire ISBN "<<endl;
+						cin>>isbn;
+						ebook.setISBN(isbn);
+						cout<<endl;
+						
+						ebook.setQuantita(1);
+						archivio.addEbook(ebook);
+						archivio.addProdotto(ebook);
+						
+						
+						
+					}
 					
+				
+					if(cond==3){
+						
+							
+						cout<<"Inserire il titolo"<<endl;
+						cin>>titolo;
+						cd.setTitolo(titolo);
+						cout<<endl;
+						cout<<"Inserire l'autore'"<<endl;
+						cin>>autore;
+						cd.setAutore(autore);
+						cout<<endl;
+						cout<<"Inserire il prezzo"<<endl;
+						cin>>prezzo;
+						cd.setPrezzo(prezzo);
+						
+						cd.setQuantita(1);
+						archivio.addCd(cd);
+						archivio.addProdotto(cd);
+						
+						cout<<"Inserire il titolo"<<endl;
+						cin>>titolo;
+						canzone.setTitolo(titolo);
+						cout<<endl;
+						cout<<"Inserire l'autore'"<<endl;
+						cin>>autore;
+						canzone.setAutore(autore);
+						cout<<endl;
+						cout<<"Inserire la durata"<<endl;
+						cin>>durata;
+						canzone.setDurata(durata);
+						
+						cd.addCanzoni(canzone);
+						
+						
+					}
 					
+					if(cond==4){
+						
+						cout<<"Inserire il titolo"<<endl;
+						cin>>titolo;
+						vinile.setTitolo(titolo);
+						cout<<endl;
+						cout<<"Inserire l'autore'"<<endl;
+						cin>>autore;
+						vinile.setAutore(autore);
+						cout<<endl;
+						cout<<"Inserire il prezzo"<<endl;
+						cin>>prezzo;
+						vinile.setPrezzo(prezzo);
+						cout<<endl;
+						cout<<"Inserire le dimensioni "<<endl;
+						cin>>dimensioni;
+						vinile.setDimensione(dimensioni);
+						cout<<endl;
+						
+						vinile.setQuantita(1);
+						archivio.addVinile(vinile);
+						archivio.addProdotto(vinile);
+						
+					}
+					else if(cond>4 || cond<1){
+						
+						cout<<"Opzione non Disponibile"<<endl;
+					}
+				
+					system("CLS");
+					
+						
 				break;
+				
+			
+				
 				
 				case 2:
 				
-					archivio.stampaProdotti();
+					cout<<"Inserire il filtro di categoria 1(libro) 2(ebook) 3(cd) 4(vinile)"<<endl;
+					cin>>condizione;
+				
+					archivio.stampaProdotti(condizione);
 					
 				break;
 				
@@ -85,6 +231,7 @@ int main(){
 						cout<<"1 Ricerca per Titolo    "<<endl;
 						cout<<"2 Ricerca per Autore    "<<endl;
 						cout<<"3 Ricerca per Prezzo    "<<endl;
+						cout<<"4 Ricerca per ISBN      "<<endl;
 						cout<<"------------------------"<<endl;
 						cout<<endl;
 						cout<<"Inserire la scelta 1|3"<<endl;
@@ -118,6 +265,8 @@ int main(){
 							 	res=archivio.ricercaPrezzo(prezzo);
 								res.stampa();	
 							break;	
+							
+		
 						}
 				
 				break;
@@ -128,11 +277,14 @@ int main(){
 					cout<<"Inserire il titolo del prodotto da eliminare: "<<endl;
 					cin>>t;
 				
-					archivio.rimuoviProdotto(t);
+					archivio.removeProdotto(t);
 				
 				break;
 					
 			}
+
+			system("PAUSE");
+			system("CLS");
 
 		
 		cout<<"Vuoi continuare 1(si) 0(no)"<<endl;
